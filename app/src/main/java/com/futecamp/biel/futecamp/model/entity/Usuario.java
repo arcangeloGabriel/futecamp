40c28134a0865a68e380036ce374b013756b6f72
arcangelos2 <32842977+arcangelos2@users.noreply.github.com>
2018-03-12 22:18:13 -0300

    teste

--- app/src/main/java/com/futecamp/biel/futecamp/model/entity/Usuario.java
+++ app/src/main/java/com/futecamp/biel/futecamp/model/entity/Usuario.java
@@ -7,4 +7,72 @@ import java.io.Serializable;
  */
 
 public class Usuario implements Serializable{
+
+    private Long id;
+    private String nome;
+    private String telefone;
+    private String email;
+    private String senha;
+    private String endereco;
+    private String foto;
+
+
+    @Override
+    public String toString() {return nome;}
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+
+    public String getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }
+
+    public String getFoto() {
+        return foto;
+    }
+
+    public void setFoto(String foto) {
+        this.foto = foto;
+    }
 }
